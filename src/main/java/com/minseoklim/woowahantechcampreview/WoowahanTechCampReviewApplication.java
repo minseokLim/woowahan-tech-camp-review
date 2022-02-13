17c2148c1106544991b966cf5e7e3e8b1f9bea6b
minseoklim <ddalgu2@gmail.com>
2022-02-13 18:08:03 +0900

    First commit

---
+++ src/main/java/com/minseoklim/woowahantechcampreview/WoowahanTechCampReviewApplication.java
@@ -0,0 +1,13 @@
+package com.minseoklim.woowahantechcampreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanTechCampReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowahanTechCampReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minseoklim/woowahantechcampreview/WoowahanTechCampReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseoklim.woowahantechcampreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowahanTechCampReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
